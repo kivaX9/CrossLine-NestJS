c1f84875859a73566155313c56879403dda8634a
kivaX9 <kirill.vlasov.05@inbox.tu>
2024-05-26 15:18:54 +0500

    защита от подделок межсайтовых запросов

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
+import * as csurf from 'csurf';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // Защита от подделок межсайтовых запросов
+  app.use(csurf());
 }
 bootstrap();
